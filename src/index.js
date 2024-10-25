420bd7116f6a94fc90fcf704c5cc710a3a368a95
ndakalajulius <juliusndakala98@gmail.com>
2024-10-25 22:31:21 +0300

    added user constant

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ reportWebVitals();
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { store } from './store'; // Ensure the correct path to your store
+import { store } from './store'; 
 import App from './App';
 
 ReactDOM.render(
